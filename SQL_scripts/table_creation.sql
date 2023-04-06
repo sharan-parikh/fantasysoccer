fc73d51dc0910a49f7624f0b28503db765286f1b
Utsav Nandi <utsav.nandi@gmail.com>
2023-04-06 16:24:42 -0400

    basic sql tables, er diagram

--- SQL_scripts/table_creation.sql
+++ SQL_scripts/table_creation.sql
@@ -1,4 +1,34 @@
-CREATE DATABASE IF NOT EXISTS `FantasySoccer`;
+DROP DATABASE IF EXISTS FantasySoccer; -- to be commented out later
+CREATE DATABASE IF NOT EXISTS FantasySoccer;
 
 USE `FantasySoccer`;
+
+-- table creation:
+
+CREATE TABLE users
+(
+    username VARCHAR(160) PRIMARY KEY,
+    first_name VARCHAR(160) NOT NULL,
+    last_name VARCHAR(160) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    password BINARY(64) NOT NULL
+);
+
+CREATE TABLE country
+(
+	name VARCHAR(32) PRIMARY KEY
+);
+
+CREATE TABLE player_position
+(
+	position_name VARCHAR(16) PRIMARY KEY
+);
+
+CREATE TABLE venue
+(
+	venue_name VARCHAR(32) PRIMARY KEY,
+    venue_country VARCHAR(32) NOT NULL,
+    venue_capacity INT
+);
+
 
