599f19b889ee77a71f23a29ed15325e2ab201347
Utsav Nandi <utsav.nandi@gmail.com>
2023-04-12 19:45:33 -0400

    added more tables

--- SQL_scripts/table_creation.sql
+++ SQL_scripts/table_creation.sql
@@ -3,25 +3,22 @@ CREATE DATABASE IF NOT EXISTS FantasySoccer;
 
 USE `FantasySoccer`;
 
--- table creation:
+-- table creation
 
+-- user related
 CREATE TABLE users
 (
-    username VARCHAR(160) PRIMARY KEY,
-    first_name VARCHAR(160) NOT NULL,
-    last_name VARCHAR(160) NOT NULL,
+    username VARCHAR(32) PRIMARY KEY,
+    first_name VARCHAR(32) NOT NULL,
+    last_name VARCHAR(32) NOT NULL,
     date_of_birth DATE NOT NULL,
-    password BINARY(64) NOT NULL
+    password VARCHAR(32) NOT NULL
 );
 
+-- miscellaneous
 CREATE TABLE country
 (
 	name VARCHAR(32) PRIMARY KEY
-);
-
-CREATE TABLE player_position
-(
-	position_name VARCHAR(16) PRIMARY KEY
 );
 
 CREATE TABLE venue
@@ -31,4 +28,39 @@ CREATE TABLE venue
     venue_capacity INT
 );
 
+-- player & team related
+CREATE TABLE player_position
+(
+	position_name VARCHAR(16) PRIMARY KEY
+);
+
+CREATE TABLE physical_team
+(
+	team_name VARCHAR(32) PRIMARY KEY
+);
+
+CREATE TABLE virtual_team
+(
+	username VARCHAR(32) UNIQUE NOT NULL,
+    team_name VARCHAR(160) UNIQUE NOT NULL,
+    CONSTRAINT virtual_team_username_fk
+		FOREIGN KEY (username)
+		REFERENCES users (username),
+	CONSTRAINT virtual_team_pk
+		PRIMARY KEY (username, team_name)
+);
+
+CREATE TABLE physical_player
+(
+	player_id VARCHAR(32) PRIMARY KEY,
+    physical_team_name VARCHAR(32) UNIQUE NOT NULL,
+    position_name VARCHAR(16) NOT NULL,
+    virtual_player_price INT NOT NULL,
+    CONSTRAINT physical_player_team_name_fk
+		FOREIGN KEY (physical_team_name)
+		REFERENCES physical_team (team_name),
+	CONSTRAINT physical_player_position_name_fk
+		FOREIGN KEY (position_name)
+		REFERENCES player_position (position_name)
+);
 
