36eed6910b0832b5193904e615c8f0db005b38d6
Utsav Nandi <utsav.nandi@gmail.com>
2023-04-14 19:15:26 -0400

    added venue insert

--- SQL_scripts/table_creation.sql
+++ SQL_scripts/table_creation.sql
@@ -4,10 +4,7 @@ CREATE DATABASE IF NOT EXISTS FantasySoccer;
 USE `FantasySoccer`;
 
 -- table creation
-
--- user related
-CREATE TABLE users
-(
+CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(32) PRIMARY KEY,
     first_name VARCHAR(32) NOT NULL,
     last_name VARCHAR(32) NOT NULL,
@@ -16,51 +13,44 @@ CREATE TABLE users
 );
 
 -- miscellaneous
-CREATE TABLE country
-(
-	name VARCHAR(32) PRIMARY KEY
+CREATE TABLE IF NOT EXISTS country (
+    name VARCHAR(100) PRIMARY KEY,
+    code VARCHAR(100) UNIQUE NOT NULL
 );
 
-CREATE TABLE venue
-(
-	venue_name VARCHAR(32) PRIMARY KEY,
-    venue_country VARCHAR(32) NOT NULL,
-    venue_capacity INT
+CREATE TABLE IF NOT EXISTS venue (
+    id INT PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    address VARCHAR(256),
+    country VARCHAR(32) NOT NULL,
+    capacity INT
 );
 
 -- player & team related
-CREATE TABLE player_position
-(
-	position_name VARCHAR(16) PRIMARY KEY
+CREATE TABLE IF NOT EXISTS player_position (
+    position_name VARCHAR(16) PRIMARY KEY
 );
 
-CREATE TABLE physical_team
-(
-	team_name VARCHAR(32) PRIMARY KEY
+CREATE TABLE IF NOT EXISTS physical_team (
+    team_name VARCHAR(32) PRIMARY KEY
 );
 
-CREATE TABLE virtual_team
-(
-	username VARCHAR(32) UNIQUE NOT NULL,
+CREATE TABLE IF NOT EXISTS virtual_team (
+    username VARCHAR(32) UNIQUE NOT NULL,
     team_name VARCHAR(160) UNIQUE NOT NULL,
-    CONSTRAINT virtual_team_username_fk
-		FOREIGN KEY (username)
-		REFERENCES users (username),
-	CONSTRAINT virtual_team_pk
-		PRIMARY KEY (username, team_name)
+    CONSTRAINT virtual_team_username_fk FOREIGN KEY (username)
+        REFERENCES users (username),
+    CONSTRAINT virtual_team_pk PRIMARY KEY (username, team_name)
 );
 
-CREATE TABLE physical_player
-(
-	player_id VARCHAR(32) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS physical_player (
+    player_id VARCHAR(32) PRIMARY KEY,
     physical_team_name VARCHAR(32) UNIQUE NOT NULL,
     position_name VARCHAR(16) NOT NULL,
     virtual_player_price INT NOT NULL,
-    CONSTRAINT physical_player_team_name_fk
-		FOREIGN KEY (physical_team_name)
-		REFERENCES physical_team (team_name),
-	CONSTRAINT physical_player_position_name_fk
-		FOREIGN KEY (position_name)
-		REFERENCES player_position (position_name)
+    CONSTRAINT physical_player_team_name_fk FOREIGN KEY (physical_team_name)
+        REFERENCES physical_team (team_name),
+    CONSTRAINT physical_player_position_name_fk FOREIGN KEY (position_name)
+        REFERENCES player_position (position_name)
 );
 
---
+++ python_scripts/insert_venue_data.py
@@ -0,0 +1,36 @@
+from mysql.connector import connect, Error
+import json
+from pprint import pprint
+
+def read_json(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+        return data
+        
+
+def enter_data(connection, filename):
+    data = read_json(filename)
+    for venue in data['venues']:
+        with connection.cursor() as cursor:
+            cursor.execute(f"INSERT INTO venue (id, name, address, country, capacity) VALUES ({venue['id']}, '{venue['name']}', '{venue['address']}', '{venue['country']}', {venue['capacity']})")
+            pprint(venue['name'])
+    connection.commit()
+
+
+
+
+if __name__ == '__main__':
+    try:
+        with connect(
+            host="localhost",
+            user="utsav",
+            password="nandi",
+            database='FantasySoccer'
+        ) as connection:
+            print('connected successfully!')
+            print(connection)
+            enter_data(connection=connection, 
+                       filename='data/venues.json')
+            connection.close()
+    except Error as e:
+        print(e)
