38c582961cca18389b8baa6f8edf2246ec5c78a1
sharan parikh <parikh.shar@northeastern.edu>
2023-04-20 01:23:17 -0400

    Updated exisiting sp and added new sp for squad update

--- SQL_scripts/add_user_sp.sql
+++ SQL_scripts/add_user_sp.sql
@@ -6,11 +6,25 @@ CREATE PROCEDURE add_user(
     IN firstname varchar(64),
     IN lastname varchar(64),
     IN dob date,
-    IN pwd varchar(50)
+    IN pwd varchar(50),
+    IN virtual_team_name varchar(50)
 )
-begin
-	DECLARE EXIT HANDLER FOR 1062 SELECT 'username already exists' Message;
-	DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQLException encountered' Message;
-	INSERT INTO users (username, first_name, last_name, date_of_birth, password) VALUES (email, firstname, lastname, dob, pwd);
-end$$
+BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, 
+			@errno = MYSQL_ERRNO, @text = MESSAGE_TEXT;
+			SET @full_error = CONCAT("ERROR ", @errno, " (", @sqlstate, "): ", @text);
+			SELECT @full_error;
+			ROLLBACK;
+		END;
+    
+    START TRANSACTION;
+		SET @virtual_team_name_exist = ( SELECT team_name FROM virtual_team WHERE team_name = virtual_team_name );
+		IF(@virtual_team_name_exist IS NOT NULL) THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'team name already exists.';
+		END IF;
+		INSERT INTO users (username, first_name, last_name, date_of_birth, password) VALUES (email, firstname, lastname, dob, pwd);
+		INSERT INTO virtual_team (username, team_name, remaining_budget) VALUES (email, virtual_team_name, 100);
+	COMMIT;
+END$$
 delimiter ;
